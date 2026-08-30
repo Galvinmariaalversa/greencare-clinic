f28b00c0b45d6539125a4cb7069644fe49e42b6a
Galvinmariaalversa <galvin.ma.j@gmail.com>
2026-08-30 18:33:44 +0530

    Improve responsive container layout

--- src/components/layout/Container.jsx
+++ src/components/layout/Container.jsx
@@ -1,13 +1,14 @@
 function Container({ children, className = "" }) {
   return (
     <div
-      className={`mx-auto w-full max-w-7xl px-4 sm:px-6 lg:px-8 ${className}`}
+      className={[
+        "mx-auto w-full max-w-7xl",
+        "px-4 sm:px-6 lg:px-8",
+        className,
+      ].join(" ")}
     >
-      <div className="mx-auto w-full max-w-7xl px-4 sm:px-6 lg:px-8"></div>
-
       {children}
     </div>
-    
   );
 }
 
